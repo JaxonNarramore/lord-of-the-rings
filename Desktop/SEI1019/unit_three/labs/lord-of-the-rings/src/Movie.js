83a2e484994aa4bd16d1020a4c88196fa804f9be
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2020-12-09 08:44:08 -0600

    [app.js] Add class method

--- Desktop/SEI1019/unit_three/labs/lord-of-the-rings/src/Movie.js
+++ Desktop/SEI1019/unit_three/labs/lord-of-the-rings/src/Movie.js
@@ -1,10 +1,25 @@
-function Movie(props) {
-    return (
+import React, { Component } from "react";
+
+// function Movie(props) {
+//     return (
+//         <div>
+//             <h1>Lord of the rings: {props.title}</h1>
+//     <p>Runtime: {props.hours} Hours {props.minuets} Minuets</p>
+//         </div>
+//     );
+// }
+
+class Movie extends Component {
+    render() {
+        return(
         <div>
-            <h1>Title: {props.title}</h1>
-    <p>Runtime: {props.hours} Hours {props.minuets} Minuets</p>
+            <h1>Lord of the rings: {this.props.title}</h1>
+    <p>Runtime: {this.props.hours} Hours {this.props.minuets} Minuets</p>
         </div>
-    );
+        );
+    }
 }
+
+
 
 export default Movie;
